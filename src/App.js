1d63989b4a90559f16a90dc91b0bb0df1a7106bc
mia-chou <mia.fy.chou@hotmail.com>
2024-04-01 06:02:45 +0100

    Fixed errors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-
 import './App.css';
+import { useState } from 'react';
+import { Modal } from './Components/Modal';
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <div className="App">
- <h1>Hey, click on the button to open the modal.</h1>
+      <h1>Hey, click on the button to open the modal.</h1>
 
- <button
-        className="openModalBtn"
-        onClick={() => {
-          setModalOpen(true);
-        }}
-      >
-        Open
-      </button>
-
+      <button className="openModalBtn" onClick={handleOpenModal}>Open</button>
+      
+      {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} />}
     </div>
   );
 }
---
+++ src/Components/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Modal = () => {
+  return (
+  <div className="modalBackground">
+  <div className="modalContainer">
+   <button>X</button>
+
+   <div className="title">
+          <h1>Are You Sure You Want to Continue?</h1>
+        </div>
+        <div className="body">
+          <p>The next page looks amazing. Hope you want to go there!</p>
+        </div>
+
+
+  </div>
+  </div>    
+);
+
+
+}
+
+export default Modal;
