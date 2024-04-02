845fe43e12dd4a9c904109be07b9421d936f081c
mia-chou <mia.fy.chou@hotmail.com>
2024-04-02 08:45:18 +0100

    Made buttons work

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Modal.css";
 
-function Modal({ setOpenModal = () => {}, onCancel = () => {}, onContinue = () => {} }) {
+function Modal({ setOpenModal, onCancel, onContinue }) {
+  const handleCancel = () => {
+    setOpenModal(false);
+    onCancel();
+  };
+
+  const handleContinue = () => {
+    setOpenModal(false);
+    onContinue();
+  };
+
   return (
     <div className="modalBackground">
       <div className="modalContainer">
@@ -15,8 +25,10 @@ function Modal({ setOpenModal = () => {}, onCancel = () => {}, onContinue = () =
           <p>I hope you want to go there! The next page looks amazing.</p>
         </div>
         <div className="footer">
-          <button onClick={onCancel} id="cancelBtn">Cancel</button>
-          <button onClick={onContinue}>Continue</button>
+          <button onClick={handleCancel} id="cancellBtn">
+            Cancel
+          </button>
+          <button onClick={handleContinue}>Continue</button>
         </div>
       </div>
     </div>
